14d2e433d1bf6b53b8b731a3b5ded8bebd1457e7
Williams Hernandez <hernandw@gmail.com>
2024-10-28 17:34:19 -0300

    :tada: mi primer proyecto en react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+const App = () => {
+
+  //aqui las funciones y/o variables
+  const nombre = 'Williams Hernández'
+  return (
+    <div>
+      <p>Mi nombre es: {nombre}</p>
+      <p>La suma de 2 + 2 es: {2 + 2}</p>
+    </div>
+  )
+}
+
+export default App
